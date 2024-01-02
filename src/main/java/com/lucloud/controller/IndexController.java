bbacc415982e7d186f7f8d93834ce9015fe91414
XiaoLuYa7 <3232414647@qq.com>
2024-01-02 16:54:16 +0800

    工具类

---
+++ src/main/java/com/lucloud/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.lucloud.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author lucky
+ * @description 初始化Controller
+ * @date 2024/1/2
+ */
+@Controller
+@RequestMapping("/index")
+public class IndexController {
+
+}
